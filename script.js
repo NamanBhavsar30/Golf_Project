7a9323874783e849391aac9c81e418ab2b2cf343
Naman <bnaman.3010@gmail.com>
2024-04-16 08:25:34 +0530

    commit

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-var crsr=document.querySelector("#cursor")
-var blur=document.querySelector("#cursor-blur")
+var crsr= document.querySelector("#cursor")
+var blur= document.querySelector("#cursor-blur")
 
 document.addEventListener("mousemove",function(dets){
-      crsr.style.left=dets.x+"px"
-      crsr.style.top=dets.y+"px"
-      blur.style.left=dets.x-150+"px"
-      blur.style.top=dets.y-150+"px"
+    crsr.style.left= dets.x+"px"
+    crsr.style.top= dets.y+"px"
+    blur.style.left= dets.x-150+"px"
+    blur.style.top= dets.y-150+"px"
 })
 
-
 gsap.to(".nav",{
-    backgroundColor:"#000", 
+    backgroundColor:"#000",
     duration:0.5,
-    height:"80px",
+    height:"70px",
     scrollTrigger:{
         trigger:".nav",
-        scroller:"body",
-        start:"top -10%",
+        scroll:"body", 
+        start:"top -10%", 
         end:"top -11%",
         scrub:2
     }
+
 })
 
 gsap.to("#main",{
@@ -28,7 +28,8 @@ gsap.to("#main",{
         trigger:"#main",
         scroll:"body",
         start:"top -30%",
-        end:"top -100%", 
+        end:"top -80%",
         scrub:2
+
     }
 })
