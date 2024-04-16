83f5d9938755a69acacfa8bfdb1eb0a2ebfaae33
Naman <bnaman.3010@gmail.com>
2024-04-16 08:11:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# golf-Project
+ It's a Web Application User Interface of a Golf Club. 
---
+++ script.js
@@ -0,0 +1,34 @@
+var crsr=document.querySelector("#cursor")
+var blur=document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove",function(dets){
+      crsr.style.left=dets.x+"px"
+      crsr.style.top=dets.y+"px"
+      blur.style.left=dets.x-150+"px"
+      blur.style.top=dets.y-150+"px"
+})
+
+
+gsap.to(".nav",{
+    backgroundColor:"#000", 
+    duration:0.5,
+    height:"80px",
+    scrollTrigger:{
+        trigger:".nav",
+        scroller:"body",
+        start:"top -10%",
+        end:"top -11%",
+        scrub:2
+    }
+})
+
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroll:"body",
+        start:"top -30%",
+        end:"top -100%", 
+        scrub:2
+    }
+})
